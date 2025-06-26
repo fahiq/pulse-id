48d0cee635b27491ee74f3fecce30d43bb777b34
@rzrz52 <rzrz52@gmail.com>
2025-06-26 11:43:38 +0530

    fix:unused imports removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import MainPage from './pages/mainPage';
-import cityOfferLanding from './pages/cityOfferLanding';
-import cityOfferDetailLanding from './pages/cityOfferDetailLanding'
+
 
 export default function RootPage() {
   return <MainPage />;
